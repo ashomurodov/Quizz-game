321f8a35e936c1945b53955dd22be44076c6aa71
ashomurodov <ayyuubshomurodov@gmail.com>
2023-03-30 16:04:43 +0500

    feat: added time, score and more

--- next.js
+++ next.js
@@ -2,14 +2,62 @@ const quizContainer = document.querySelector(".quiz_container");
 let questionEl = [];
 let answerEl = [];
 let operators = ["+", "-", "*"];
-let operator;
-let hasAnswer = false;
-let correctAnswer;
-let correctIndex;
+let operator,
+  correctAnswer,
+  correctIndex,
+  interval,
+  hasAnswer,
+  permissionForClick;
+let NUMBER_OF_QUESTIONS = 10;
+let NUMBER_OF_LEVEL = 1;
+let BONUS = 200;
+let earnedBonus = 0;
+let correctQuestions = 0;
+let seconds = 10;
+
+const newGame = document.getElementById("new_game");
+const overLay = document.querySelector(".overlay");
+const startGameoverLay = document.querySelector(".startGameOverlay");
+const startGame = document.getElementById("start_game");
+
+startGameoverLay.classList.remove("none");
+
+const time = document.getElementById("time");
+const bonus_in_time = document.getElementById("bonusBytime");
+const bonus = document.getElementById("bonus");
+const level = document.getElementById("level");
+
+startGame.addEventListener("click", () => {
+  startGameoverLay.classList.add("none");
+  renderGame();
+});
+
+function getTime() {
+  let currentecond = seconds;
+  let currentBonus = BONUS;
+  if (seconds == 0) {
+    clearInterval(interval);
+    nextLevel();
+  }
+  seconds--;
+  BONUS -= 20;
+  time.textContent = `time: 00:${
+    currentecond >= 10 ? currentecond : "0" + currentecond
+  }`;
+  bonus_in_time.textContent = `level bonus: ${currentBonus}`;
+}
+
+function timeGO() {
+  interval = setInterval(getTime, 1000);
+}
+
 function renderGame() {
+  timeGO();
+
   questionEl = [];
   answerEl = [];
   hasAnswer = false;
+  permissionForClick = true;
   [...quizContainer.children].forEach((elm) => elm.remove());
   const questionSection = document.createElement("div");
   questionSection.className = "question-section";
@@ -41,18 +89,12 @@ function renderGame() {
     correctAnswer + 1,
   ];
 
-  console.log(correctAnswer);
-
   answersArray = answersArray.sort((a, b) => Math.random() - 0.5);
   answersArray.filter((item, idx) => {
     if (item === correctAnswer) {
       correctIndex = idx;
     }
   });
-
-  console.log(answersArray);
-
-  console.log(correctIndex);
 
   const answerSection = document.createElement("div");
   answerSection.className = "answers-section";
@@ -69,17 +111,59 @@ function renderGame() {
 }
 
 function handleBtn(e) {
-  let elm = e.target;
-  if (elm.textContent == correctAnswer) {
-    elm.classList.add("correct");
-    setTimeout(renderGame, 1000);
-  } else {
-    elm.classList.add("failed");
-    answerEl[correctIndex].classList.add("correct");
-    setTimeout(renderGame, 1000);
+  if (permissionForClick) {
+    permissionForClick = false;
+    let elm = e.target;
+    if (elm.textContent == correctAnswer) {
+      elm.classList.add("correct");
+      correctQuestions++;
+      console.log(correctQuestions);
+      setTimeout(() => {
+        nextLevel();
+      }, 1000);
+    } else {
+      elm.classList.add("failed");
+      answerEl[correctIndex].classList.add("correct");
+      setTimeout(() => {
+        BONUS = 0;
+        nextLevel();
+      }, 1000);
+    }
   }
 }
 
-renderGame();
+function nextLevel() {
+  clearInterval(interval);
+  seconds = 10;
+  earnedBonus += BONUS;
+  BONUS = 200;
+  NUMBER_OF_LEVEL !== 10 && renderGame();
+  NUMBER_OF_LEVEL++;
+  if (NUMBER_OF_LEVEL == 11) resultOfGame();
+  bonus.textContent = `your bonus: ${earnedBonus}`;
+  level.textContent = `level: ${
+    NUMBER_OF_LEVEL == 11 ? 10 : NUMBER_OF_LEVEL
+  }/10`;
+}
 
-console.log(questionEl, correctAnswer);
+function resultOfGame() {
+  overLay.classList.remove("none");
+  const RESULT_ANSWERS = document.querySelector(".correctAnswers");
+  const RESULT_BONUS = document.querySelector(".resultBonus");
+
+  RESULT_ANSWERS.textContent = `correct answers: ${correctQuestions}/10`;
+  RESULT_BONUS.textContent = `earned bonus: ${earnedBonus}`;
+}
+
+newGame.addEventListener("click", () => {
+  seconds = 10;
+  NUMBER_OF_LEVEL = 1;
+  earnedBonus = 0;
+  correctQuestions = 0;
+  BONUS = 200;
+  bonus.textContent = `your bonus: 0`;
+  time.textContent = "00:05";
+  level.textContent = "level: 1/10";
+  overLay.classList.add("none");
+  renderGame();
+});
